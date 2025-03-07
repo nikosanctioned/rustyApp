d490624d52df7ce0297fd6d50dd88dda6b2ba0c6
Nikolai <Nikolay.Polozov@lemanapro.ru>
2025-03-07 17:01:42 +0300

    Exclude target folder in .gitignore

--- minigrep/target/debug/deps/minigrep-9877209315e66791.d
+++
@@ -1,5 +0,0 @@
-/Users/nikolaypolozov/Documents/GitHub/rustyApp/minigrep/target/debug/deps/minigrep-9877209315e66791: src/main.rs
-
-/Users/nikolaypolozov/Documents/GitHub/rustyApp/minigrep/target/debug/deps/minigrep-9877209315e66791.d: src/main.rs
-
-src/main.rs:
--- minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/Users/nikolaypolozov/Documents/GitHub/rustyApp/minigrep/target/debug/minigrep: /Users/nikolaypolozov/Documents/GitHub/rustyApp/minigrep/src/main.rs
